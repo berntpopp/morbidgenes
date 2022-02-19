b9b947185bdfed3421d48acef8e7b029b7bb2aae
berntpopp <bernt.popp@googlemail.com>
2022-02-19 11:38:49 +0100

    config changes

--- api/start_morbidgenes-db_api_development.R
+++ api/start_morbidgenes-db_api_development.R
@@ -1,6 +1,6 @@
 library(plumber)
 
-setwd("C:/development/HNF1B-db/api")
+setwd("C:/development/morbidgenes/api")
 
 root <- pr("morbidgenes-db_plumber.R") %>%
         pr_run(host = "0.0.0.0", port = 10018) %>%
