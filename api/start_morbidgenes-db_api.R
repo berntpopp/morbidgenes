15589bee02b185909b9d3190b5805083da434936
berntpopp <bernt.popp@googlemail.com>
2022-02-19 15:47:41 +0100

    api port config

--- api/start_morbidgenes-db_api.R
+++ api/start_morbidgenes-db_api.R
@@ -3,5 +3,5 @@ library(plumber)
 setwd("/morbidgenes_api_volume")
 
 root <- pr("morbidgenes-db_plumber.R") %>%
-        pr_run(host = "0.0.0.0", port = 10018) %>%
+        pr_run(host = "0.0.0.0", port = 10918) %>%
 		pr_hook("exit", function(){ poolClose(pool) })
--- api/start_morbidgenes-db_api_development.R
+++ api/start_morbidgenes-db_api_development.R
@@ -3,5 +3,5 @@ library(plumber)
 setwd("C:/development/morbidgenes/api")
 
 root <- pr("morbidgenes-db_plumber.R") %>%
-        pr_run(host = "0.0.0.0", port = 10018) %>%
+        pr_run(host = "0.0.0.0", port = 10918) %>%
 		pr_hook("exit", function(){ poolClose(pool) })
