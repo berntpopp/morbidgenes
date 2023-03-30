c551e095e59a8e0c28500f521b55713b1e8f43ea
berntpopp <bernt.popp@googlemail.com>
2023-03-30 12:19:08 +0200

    update api start scripts

--- api/start_morbidgenes-db_api.R
+++
@@ -1,7 +0,0 @@
-setwd("/morbidgenes_api_volume")
-
-library(plumber)
-
-root <- pr("morbidgenes-db_plumber.R") %>%
-        pr_run(host = "0.0.0.0", port = 10918) %>%
-		pr_hook("exit", function(){ poolClose(pool) })
