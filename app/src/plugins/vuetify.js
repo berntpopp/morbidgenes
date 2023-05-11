82f57dc726f894e6abcec0bdbbeaca799d4e6538
berntpopp <bernt.popp@googlemail.com>
2023-05-11 09:42:50 +0200

    ad nonce

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -7,4 +7,8 @@ export default new Vuetify({
     icons: {
       iconfont: 'mdiSvg', // default - only for display purposes
     },
+    // based on https://v2.vuetifyjs.com/en/features/theme/#custom-properties
+    theme: {
+      options: { cspNonce: 'dQw4w9WgXcQ' },
+    },
   });
